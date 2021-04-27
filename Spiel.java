bfed6f4a8ab350bad3a1914f5a8b98180c210e8f
Paul Görner <pgoerner@outlook.de>
2021-04-27 09:45:29 +0200

    Klasse Spiel wurde hinzugefügt.

---
+++ Spiel.java
@@ -0,0 +1,15 @@
+/*
+ * Organisiert das Spiel
+ * 
+ * von Joel und Paul
+ */
+
+public class Spiel
+{
+    boolean spielGestartet = false;
+    boolean spielVerloren = false;
+    
+    public Spiel(boolean spielGestartetNeu, ){
+    
+    }
+}
