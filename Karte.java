c158e5ea3af9317858eaf12af3c36830d032915a
Paul Görner <pgoerner@outlook.de>
2021-07-02 12:14:22 +0200

    Karten erzeugen

--- Karte.java
+++ Karte.java
@@ -5,17 +5,11 @@
  *  Von: Paul, Sachsa, Julian
  */
 
-import  java.awt.*;
-import javax.swing.*;
-
 public class Karte
 {
     private static int KREUZ=0, PIK=1, HERZ=2, KARO=3;
     private static int BUBE=11, DAME=12, KOENIG=13, ASS=1;
     private int index, farbe;
-    
-    public JFrame kartenbild;
-    private Image image;
     
     /*
      * Konstruktor der Klasse Karte:
@@ -35,15 +29,5 @@ public class Karte
     // Methode zum Geben der Farbe
     public int farbeGeben(){
         return farbe;
-    }
-    
-    // Erzeugt Karten auf dem Spielertisch, sobald einer gezogen wurde.
-    public void kartenErzeugen(int farbe, int index)
-    {
-        Image image = Toolkit.getDefaultToolkit().getImage(getClass().getResource("\\res\\karten\\" + farbe + "-" + index + ".png"));
-        kartenbild.setIconImage(image);
-
-        kartenbild.pack();
-        
     }
 }
