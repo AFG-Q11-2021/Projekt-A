041f438f06745db34b261acd682b2e98dfd229eb
Sascha Kuckelkorn <sascha.kuckelkorn@gmx.de>
2021-07-02 12:14:28 +0200

    karten richtig bennant fürs anzeigen

--- Karte.java
+++ Karte.java
@@ -11,7 +11,7 @@ import javax.swing.*;
 public class Karte
 {
     private static int KREUZ=0, PIK=1, HERZ=2, KARO=3;
-    private static int BUBE=11, DAME=12, KOENIG=13, ASS=1;
+    private static int BUBE=11, DAME=12, KOENIG=13, ASS=1; 
     private int index, farbe;
     
     public JFrame kartenbild;
